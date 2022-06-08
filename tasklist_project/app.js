a1a08a879c212c150e4f79dd3279f6bb80bbc788
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2022-06-08 12:41:57 +0530

    add and remove task functionality

---
+++ tasklist_project/app.js
@@ -0,0 +1,62 @@
+//Define UI variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+console.log("working");
+// Load all event listeners
+loadEventListeners();
+
+//Function load all event listeners
+function loadEventListeners(){
+    //add task event
+    form.addEventListener('submit',addTask);
+    
+    //Remove task event
+    taskList.addEventListener('click',removeTask);
+}
+
+//Add Task
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Add a task');
+    }
+
+    //create li element
+    const li = document.createElement('li');
+    //add className
+    li.className = 'collection-item';
+    //create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement('a');
+    //add className
+    link.className = 'delete-item secondary-content';
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append the link to li 
+    li.appendChild(link);
+    
+    //Append the li to ul
+    console.log(li);
+    taskList.appendChild(li);
+
+    //clear the input
+    taskInput.value= '';
+
+
+
+    e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+        }
+        console.log(e.target);
+    }
+}

3d05e09031dea631e3b0e8566d4c39dfa38c9158
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2022-06-17 20:07:09 +0530

    arrow function

---
+++ Asyncronous Javascript, Ajax and Fetch API/Arrow Function/app.js
@@ -0,0 +1,20 @@
+//Normal Function
+// const greeting=function(){
+//     console.log('Good Evening');
+// }
+
+// greeting();
+
+
+
+//Arrow Function
+//Fat arrow function
+// const greeting=()=>{
+//     console.log('Good Evening');
+// }
+
+// greeting();
+
+//single return 
+const greeting=()=>console.log('Good Evening');
+greeting()

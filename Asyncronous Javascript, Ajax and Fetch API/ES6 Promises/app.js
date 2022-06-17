91c1cf4b6d56b94f77262c4bb75f3be17e8a2d2b
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2022-06-17 16:49:10 +0530

    ES6 promises

---
+++ Asyncronous Javascript, Ajax and Fetch API/ES6 Promises/app.js
@@ -0,0 +1,40 @@
+const posts = [
+    {title:"Post One", body:"This is post one"},
+    {title:"Post Two", body:"This is post two"},
+];
+
+
+
+//Withcallback (Asyncronous call)
+
+function createPost(post){
+    return new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            posts.push(post)  
+
+            const error = false;
+            if(!error){
+                resolve();
+            }else{
+                reject('Error:Something went wrong');
+            }
+        },2000);
+    })
+}
+
+function getPosts(){
+    setTimeout(()=>{
+        let output = '';
+        posts.forEach(post => {
+            output += `
+            <li>${post.title}</li>`;
+        })
+        document.body.innerHTML = output;
+    },1000);
+};
+
+createPost({title:"Post Three",body:"This is post three"})
+.then(getPosts)
+.catch(function(err){
+    console.log(err);
+});
